8feec90116439a76af82b407d3db7393babed22b
Admin <admin@Admins-MacBook-Air.local>
2024-09-15 19:30:04 +0100

    Testing Lefthook

--- src/app/page.spec.tsx
+++ src/app/page.spec.tsx
@@ -2,11 +2,11 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Home from './page';
 
-describe('Home', () => {
-  it('renders a heading', () => {
+describe("Home", () => {
+  it("renders a heading", () => {
     render(<Home />);
 
-    const word = screen.getByText('Save and see your changes instantly.');
+    const word = screen.getByText("Save and see your changes instantly.");
     expect(word).toBeInTheDocument();
   });
 });
