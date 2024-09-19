085794c037b9590a779304985f3aaeb8a0ef6fb8
Admin <admin@Admins-MacBook-Air.local>
2024-09-19 15:03:23 +0100

    Testing CI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import styles from './page.module.css';
 
 export default function Home() {
-  // COMMENT HERE
   return (
-    <div className={styles.page}>
-      <ol>
-        <li>
-          Get started by editing <code>src/app/page.tsx</code>.
-        </li>
-        <li>Save and see your changes instantly.</li>
-      </ol>
-    </div>
+<div className={styles.page}>
+<ol>
+<li>
+Get started by editing <code>src/app/page.tsx</code>.
+</li>
+<li>Save and see your changes instantly.</li>
+</ol>
+</div>
   );
 }
