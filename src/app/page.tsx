4be73759c81d614986525aa298931ae1a9edf62a
Admin <admin@Admins-MacBook-Air.local>
2024-09-19 15:22:11 +0100

    Testing CI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 <div className={styles.page}>
 <ol>
 <li>
-Get started by editing <code>src/app/page.tsx</code>.
+  Get started by editing <code>src/app/page.tsx</code>.
 </li>
 <li>Save and see your changes instantly.</li>
 </ol>
