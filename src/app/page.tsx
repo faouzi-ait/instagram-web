def8c106182c92f0c2f02c5ae3d14bc5dcb5f97f
Admin <admin@Admins-MacBook-Air.local>
2024-09-28 09:57:58 +0100

    Fixed PR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-<div className={styles.page}>
-<ol>
-<li>PR CHECKS!!!!!</li>
-<li>
-Get started by editing <code>src/app/page.tsx</code>.
-</li>
-<li>Save and see your changes instantly.</li>
-</ol>
-</div>
+    <div className={styles.page}>
+      <ol>
+        <li>PR CHECKS!!!!!</li>
+        <li>
+          Get started by editing <code>src/app/page.tsx</code>.
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+    </div>
   );
 }
