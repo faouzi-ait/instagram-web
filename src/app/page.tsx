f01df223bafcbff3cabd2aa218404491df9177c8
faouzi-ait <33617564+faouzi-ait@users.noreply.github.com>
2024-09-29 21:48:24 +0100

    Added Dark and Light theme with redux-toolkit (#5)
    * Added Dark and Light theme with redux-toolkit
    
    * Added comment
    
    * Added Ignore lint
    
    * Updated state type
    
    * Updated state type
    
    ---------
    
    Co-authored-by: Admin <admin@Admins-MacBook-Air.local>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,36 @@
-import styles from './page.module.css';
+'use client';
+
+import { useSelector, useDispatch } from 'react-redux';
+import type { RootState, AppDispatch } from '../redux/store';
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from '../redux/slices/counterSlice';
+import ThemeToggle from './components/ThemeToggle';
+
+// import styles from './page.module.css';
 
 export default function Home() {
+  // COMMENT HERE
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
-    <div className={styles.page}>
-      <ol>
-        <li>PR CHECKS!!!!!</li>
-        <li>OTHER BRANCH</li>
-        <li>LAST UPDATE!!!</li>
-        <li>
-          Get started by editing <code>src/app/page.tsx</code>.
-        </li>
-        <li>Save and see your changes instantly.</li>
-      </ol>
+    <div>
+      <h1>This is the home page</h1>
+      <ThemeToggle />
+      <h1>Count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button
+        onClick={() => dispatch(decrement())}
+        style={{ margin: '0 .5rem' }}
+      >
+        Decrement
+      </button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        Increment by 5
+      </button>
     </div>
   );
 }
---
+++ src/redux/slices/themeSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface ThemeState {
+  theme: string;
+}
+
+const initialState: ThemeState = {
+  theme: 'dark',
+};
+
+const themeSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    setTheme: (state, action: PayloadAction<'light' | 'dark'>) => {
+      state.theme = action.payload;
+    },
+  },
+});
+
+export const { setTheme } = themeSlice.actions;
+export default themeSlice.reducer;
