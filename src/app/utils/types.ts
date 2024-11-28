9fa84896ed3a8a45a33de971a32ca02251adcbaf
Admin <admin@Admins-MacBook-Air.local>
2024-11-28 09:42:20 +0000

    Fix

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -19,7 +19,13 @@ export interface Review {
   rating: number;
   user: string;
   username: string;
-  _id: string;
+  _id?: string;
+}
+
+export interface ReviewDisplayProps {
+  userId: string;
+  username: string;
+  comment: string;
 }
 
 export interface AuthState {
