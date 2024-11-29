b278d20828c73a530568477912cdf30f82620cde
faouzi-ait <33617564+faouzi-ait@users.noreply.github.com>
2024-11-29 00:02:35 +0000

    Added image loader component (#28)
    Co-authored-by: Admin <admin@Admins-MacBook-Air.local>

---
+++ src/app/components/atomic-components/atoms/image-with-loader/index.tsx
@@ -0,0 +1,35 @@
+import React, { Suspense } from 'react';
+
+import styles from './imageWithLoader.module.css';
+
+const LazyImage = React.lazy(() => import('next/image'));
+
+interface ImageWithLoaderProps {
+  src: string;
+  alt: string;
+  sizes?: string;
+  priority?: boolean;
+  fill?: boolean;
+  className?: string;
+}
+
+const ImageWithLoader: React.FC<ImageWithLoaderProps> = ({
+  src,
+  alt,
+  sizes = 'auto',
+  priority = false,
+  fill = false,
+  className,
+}) => {
+  return (
+    <div className={`${styles.imageWrapper} ${className || ''}`}>
+      <div className={styles.loaderContainer}>
+        <Suspense fallback={<div className={styles.loader}></div>}>
+          <LazyImage sizes={sizes} alt={alt} src={src} priority={priority} fill={fill} />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default ImageWithLoader;
